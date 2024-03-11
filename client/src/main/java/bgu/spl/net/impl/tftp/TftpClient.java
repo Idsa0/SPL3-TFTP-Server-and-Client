8036099417763556d10bda86cdcd60f01ae1bf50
Idsa0 <121605149+Idsa0@users.noreply.github.com>
2024-03-11 19:28:15 +0200

    added custom ip option

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -2,15 +2,18 @@ package bgu.spl.net.impl.tftp;
 
 public class TftpClient {
     public static void main(String[] args) {
-        String ip;
-        if (args.length == 0)
-            ip = "localhost";
-        else
+        String ip = "localhost";
+        int port = 7777;
+
+        if (args.length >= 1)
             ip = args[0];
+        
+        if (args.length >= 2)
+            port = Integer.valueOf(args[1]);
 
-        Listener listen = new Listener(ip, 7777, new TftpEncoderDecoder(), new TftpClientProtocol());
+        Listener listen = new Listener(ip, port, new TftpEncoderDecoder(), new TftpClientProtocol());
         KeyboardListener keyboardListener = new KeyboardListener(listen);
-
+        
         new Thread(listen, "Communication thread").start(); // TODO synchronized their running so they are online at the same time.
         new Thread(keyboardListener, "Keyboard thread").start();
     }
