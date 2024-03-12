7243f1c313464a0725e3b82423e88ca1f2b92e47
Idsa0 <121605149+Idsa0@users.noreply.github.com>
2024-03-12 16:13:33 +0200

    Update TftpClient.java
    hello

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -4,19 +4,16 @@ public class TftpClient {
     public static void main(String[] args) {
         String ip = "localhost";
         int port = 7777;
-        
-        if (args.length == 0) {
-            ip = "localhost";
-            port = 7777;
-        }
-        else
-            ip = args[0];
 
+        if (args.length >= 1)
+            ip = args[0];
         
-        // TODO weird null bug??
+        if (args.length >= 2)
+            port = Integer.valueOf(args[1]);
+
         Listener listen = new Listener(ip, port, new TftpEncoderDecoder(), new TftpClientProtocol());
         KeyboardListener keyboardListener = new KeyboardListener(listen);
-
+        
         new Thread(listen, "Communication thread").start(); // TODO synchronized their running so they are online at the same time.
         new Thread(keyboardListener, "Keyboard thread").start();
     }
