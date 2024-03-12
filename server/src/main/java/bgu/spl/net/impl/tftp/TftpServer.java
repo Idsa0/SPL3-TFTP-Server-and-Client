a22e37972cec54e2702a787fc637d1c5187126b9
Idsa0 <121605149+Idsa0@users.noreply.github.com>
2024-03-12 16:14:31 +0200

    Update TftpServer.java
    lalala

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -6,13 +6,12 @@ import bgu.spl.net.srv.BaseServer;
 import bgu.spl.net.srv.BlockingConnectionHandler;
 import bgu.spl.net.srv.BlockingConnections;
 import bgu.spl.net.srv.Connections;
-
 import java.io.IOException;
 import java.util.function.Supplier;
 
 public class TftpServer<T> extends BaseServer<T> {
     public TftpServer(int port, Supplier<BidiMessagingProtocol<T>> protocolFactory,
-                      Supplier<MessageEncoderDecoder<T>> encdecFactory, Connections<T> connections) {
+            Supplier<MessageEncoderDecoder<T>> encdecFactory, Connections<T> connections) {
         super(port, protocolFactory, encdecFactory, connections);
     }
 
@@ -22,10 +21,15 @@ public class TftpServer<T> extends BaseServer<T> {
     }
 
     public static void main(String[] args) {
-        TftpServer<TftpInstruction> server = new TftpServer<>(7777,
+        int port = 7777;
+
+        if (args.length >= 1)
+            port = Integer.valueOf(args[0]);
+
+        TftpServer<TftpInstruction> server = new TftpServer<TftpInstruction>(port,
                 TftpProtocol::new,
                 TftpEncoderDecoder::new,
-                new BlockingConnections());
+                (Connections<TftpInstruction>) new BlockingConnections());
 
         server.serve();
 
